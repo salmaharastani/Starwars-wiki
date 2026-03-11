4302d808c19665325d338c6a2c0de49083c09e53
andi49 <46962871+andi49@users.noreply.github.com>
2026-03-11 12:07:46 +0100

    Update navbar

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,6 +1,6 @@
 const menuToggle = document.querySelector('#menuToggle');
 const navLinks = document.querySelector('#navLinks');
-const imgStarfighter = document.querySelector('imgStarfighter')
+const imgStarfighter = document.querySelector('#imgStarfighter')
 
 menuToggle.addEventListener('click', () => {
   navLinks.classList.toggle('active');
